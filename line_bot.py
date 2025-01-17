291f1c60ba6734e429a6f8c7f9143e10f732071c
wed281 <computerking04@gmail.com>
2025-01-17 15:26:53 +0800

    更新內容

--- line_bot.py
+++ line_bot.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
@@ -18,17 +19,27 @@ def hello():
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # 取得 LINE 傳來的簽名和請求體
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
     try:
+        # 處理請求
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
     return 'OK'
 
+# 設定訊息回應邏輯
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 回應用戶發送的訊息
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='您說的是: ' + event.message.text)
+    )
+
 if __name__ == "__main__":
-    # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
