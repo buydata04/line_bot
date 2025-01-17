df4aec5317609d7ce73cf0f3b48269e417315801
wed281 <computerking04@gmail.com>
2025-01-17 13:27:22 +0800

    更新內容

--- line_bot.py
+++ line_bot.py
@@ -1,43 +1,4 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
-
-app = Flask(__name__)
-
-# 替換為你的 LINE Channel Access Token 和 Channel Secret
-LINE_CHANNEL_ACCESS_TOKEN = "yKpXw7OFwbqJxxFADaOPb4Kdii9JIcvlzXGJnmwFttMUfVF5+2GAkSiu9mANnchPy5KbBGPEJdNfQbkBiT5j5Q/Jq1GPLoK9iIx1n20CU9uG3Rqlk9nez71k5lIi1zLkjYOG8ZJcsCZoEvrVZWq3FQdB04t89/1O/w1cDnyilFU="
-LINE_CHANNEL_SECRET = "2a5ea71857e45cc5dcb325641cf0a6a9"
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    # 確認 LINE 傳來的請求是否有簽章
-    signature = request.headers["X-Line-Signature"]
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return "OK"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # 回覆用戶相同的訊息
-    reply = f"你剛剛說了：{event.message.text}"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
-
-if __name__ == "__main__":
-    # 確保 Heroku 在對應的端口運行
-    port = int(os.environ.get("PORT", 5000))  # 使用環境變數設定 port，若無則預設為 5000
-    app.run(host="0.0.0.0", port=port)
-
-
-
-from waitress import serve
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -45,30 +6,36 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-# 替換為你的 LINE Channel Access Token 和 Channel Secret
+# 必須設置為你的Channel Access Token 和 Channel Secret
 LINE_CHANNEL_ACCESS_TOKEN = "yKpXw7OFwbqJxxFADaOPb4Kdii9JIcvlzXGJnmwFttMUfVF5+2GAkSiu9mANnchPy5KbBGPEJdNfQbkBiT5j5Q/Jq1GPLoK9iIx1n20CU9uG3Rqlk9nez71k5lIi1zLkjYOG8ZJcsCZoEvrVZWq3FQdB04t89/1O/w1cDnyilFU="
 LINE_CHANNEL_SECRET = "2a5ea71857e45cc5dcb325641cf0a6a9"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
-    # 確認 LINE 傳來的請求是否有簽章
-    signature = request.headers["X-Line-Signature"]
+    # 確保請求的簽名是有效的
+    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return "OK"
 
+    return 'OK'
+
+# 定義當用戶傳送文字訊息時，機器人的回應
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 回覆用戶相同的訊息
-    reply = f"你剛剛說了：{event.message.text}"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+    text = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='你說了: ' + text)
+    )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Heroku 提供的環境變數 PORT
-    serve(app, host="0.0.0.0", port=port)
+    # 使用 Heroku 提供的 $PORT 來綁定服務
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
